ab11228463cbb1fabc674c90c3ccf717b0727083
YuriODantas <iruy3343@gmail.com>
2022-11-03 13:53:21 -0300

    feat(button): add new props

--- src/components/Botao.svelte
+++ src/components/Botao.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
   export let texto: string = ''
+  export let duplo: boolean = false
+  export let triplo: boolean = false
 </script>
 
-<button class="botao">
+<button class="botao" class:triplo class:duplo>
   {texto}
 </button>
 
 <style>
   .botao {
+    flex-basis: 25%;
+  }
 
+  .duplo {
+    flex-basis: 50%;
+  }
+
+  .triplo {
+    flex-basis: 75%;
   }
 </style>
